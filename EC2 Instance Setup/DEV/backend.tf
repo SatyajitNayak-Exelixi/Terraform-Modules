2870b727eaf80a2ee51a1a41eb7e2594dfb8f8b2
Satyajit Nayak <166747732+SatyajitNayak-Exelixi@users.noreply.github.com>
2025-11-12 16:36:51 +0530

    Add DynamoDB table for state locking

--- EC2 Instance Setup/DEV/backend.tf
+++ EC2 Instance Setup/DEV/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "my-terraform-states"
-    key     = "dev/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+    bucket         = "my-terraform-states"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
   }
 }
